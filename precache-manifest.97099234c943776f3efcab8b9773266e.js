2581d7636185f1b16f7b0854a24285db73162c14
Elijah Quesada <this_boy10@yahoo.com>
2020-02-27 07:53:50 -0800

    Updates

---
+++ precache-manifest.97099234c943776f3efcab8b9773266e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "684c34c4b34684943ad7fc892f26e1b4",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "9dd5592e95db3da80300",
+    "url": "/myapp/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "a4cda242396661e1e98b",
+    "url": "/myapp/static/css/main.ff612dc5.chunk.css"
+  },
+  {
+    "revision": "9dd5592e95db3da80300",
+    "url": "/myapp/static/js/2.fbd94eee.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/myapp/static/js/2.fbd94eee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4cda242396661e1e98b",
+    "url": "/myapp/static/js/main.93aa8699.chunk.js"
+  },
+  {
+    "revision": "f33e39fc8e29dbe19aef",
+    "url": "/myapp/static/js/runtime-main.7a5b3656.js"
+  },
+  {
+    "revision": "676f7932deb2c95b2bd1986e6b4c8283",
+    "url": "/myapp/static/media/blood.676f7932.jpg"
+  },
+  {
+    "revision": "0ee444821eda0407fa414c68439eb32d",
+    "url": "/myapp/static/media/medication.0ee44482.jpg"
+  },
+  {
+    "revision": "27671b51be35ff31c9a69762114995dd",
+    "url": "/myapp/static/media/procedures.27671b51.jpg"
+  }
+]);
